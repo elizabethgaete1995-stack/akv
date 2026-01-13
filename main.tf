2ad19fcee5a605362c2267907019814f51055667
equiroz1995 <elizabethgaete1995@gmail.com>
2026-01-13 14:57:07 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ data "azurerm_monitor_diagnostic_categories" "akv" {
 
 # Creata a Key Vault
 resource "azurerm_key_vault" "akv_sa" {
-  name                            = join("", [var.app_name, local.geo_region, var.entity,var.environment, var.sequence_number])
+  name                            = join("", [var.app_name, var.location, var.entity,var.environment, var.sequence_number])
   resource_group_name             = var.rsg_name
   location                        = local.location
   tenant_id                       = var.arm_tenant_id
