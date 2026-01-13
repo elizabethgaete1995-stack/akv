6780fc6aba4ef8ed5faa9b06e553dbb871ee1af7
equiroz1995 <elizabethgaete1995@gmail.com>
2026-01-13 15:17:01 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,13 @@
-/*
-// MODULES
-# Module whitelist
-module "module-IPwhitelist" {
-  source  = "tfe1.sgtech.corp/curated-catalog/module-irw/azurerm"
-  version = ">=1.0.0"
-}
-
-# Module regions
-module "azure_regions" {
-  source  = "tfe1.sgtech.corp/curated-catalog/module-reg/azurerm"
-  version = ">=1.0.0"
-}
-
-# Module tags
-module "tags" {
-  source  = "tfe1.sgtech.corp/curated-catalog/module-tag/azurerm"
-  version = ">=1.0.0"
-
-  rsg_name       = var.rsg_name
-  inherit        = var.inherit
-  product        = var.product
-  cost_center    = var.cost_center
-  shared_costs   = var.shared_costs
-  apm_functional = var.apm_functional
-  cia            = var.cia
-  custom_tags    = var.custom_tags
-  optional_tags  = var.optional_tags
-}
-*/
 ## Locals
 # Define variables for local scope
 locals {
   geo_region = lookup(local.regions, local.location)
-  location                   = var.location != null ? var.location : data.azurerm_resource_group.rsg_principal.location
   diagnostic_monitor_enabled = substr(var.rsg_name, 3, 1) == "p" || var.analytics_diagnostic_monitor_enabled ? true : false
   mds_lwk_enabled            = var.analytics_diagnostic_monitor_lwk_id != null || (var.lwk_name != null && local.rsg_lwk != null)
   mds_sta_enabled            = var.analytics_diagnostic_monitor_sta_id != null || (var.analytics_diagnostic_monitor_sta_name != null && var.analytics_diagnostic_monitor_sta_rsg != null)
   mds_aeh_enabled            = var.analytics_diagnostic_monitor_aeh_name != null && (var.eventhub_authorization_rule_id != null || (var.analytics_diagnostic_monitor_aeh_namespace != null && var.analytics_diagnostic_monitor_aeh_rsg != null))
   rsg_lwk  = var.lwk_rsg_name != null ? var.lwk_rsg_name : data.azurerm_resource_group.rsg_principal.name
+  location                   = var.location != null ? var.location : data.azurerm_resource_group.rsg_principal.location
 }
 
 
